0e0bf3f32daef5bf3e251f118ebbc61d7803c952
asmyczek <adam.smyczek@gmail.com>
2011-07-18 23:15:32 -0700

    Adding Avro support.

---
+++ contrib/mr-kluj/project.clj
@@ -0,0 +1,7 @@
+(defproject mk-kluj/mr-kluj "1.0.0-SNAPSHOT"
+  :description "MR-Kluj"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [simple-avro/simple-avro "0.0.4"]]
+  :disable-deps-clean true
+  :warn-on-reflection true)
