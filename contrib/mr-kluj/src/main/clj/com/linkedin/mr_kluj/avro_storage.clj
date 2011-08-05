14519c3db53af6c6d78686e0524c6f8fe0f6b26a
asmyczek <adam.smyczek@gmail.com>
2011-08-04 23:16:32 -0700

    Improving performance, adding partial deserialization, updating to simple-avro 0.5.

--- contrib/mr-kluj/src/main/clj/com/linkedin/mr_kluj/avro_storage.clj
+++ contrib/mr-kluj/src/main/clj/com/linkedin/mr_kluj/avro_storage.clj
@@ -1,79 +1,97 @@
-;Copyright 2010 LinkedIn, Inc
+; Copyright 2010 LinkedIn, Inc
 ;
-;Licensed under the Apache License, Version 2.0 (the "License"); you may not
-;use this file except in compliance with the License. You may obtain a copy of
-;the License at
+; Licensed under the Apache License, Version 2.0 (the "License"); you may not
+; use this file except in compliance with the License. You may obtain a copy of
+; the License at
 ;
-;http://www.apache.org/licenses/LICENSE-2.0
+; http://www.apache.org/licenses/LICENSE-2.0
 ;
-;Unless required by applicable law or agreed to in writing, software
-;distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-;WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-;License for the specific language governing permissions and limitations under
-;the License.
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+; WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+; License for the specific language governing permissions and limitations under
+; the License.
 
 (ns com.linkedin.mr-kluj.avro-storage
   (:require [com.linkedin.mr-kluj.job :as job]
             [simple-avro.core :as avro]
             [simple-avro.utils :as avro-utils]
             [clojure.contrib.str-utils2 :as str-utils])
-	(:import (com.linkedin.json LatestExpansionFunction))
-  (:import (org.apache.hadoop.conf Configuration))
-  (:import (org.apache.hadoop.mapreduce Job InputSplit TaskAttemptContext RecordWriter))
-	(:import (org.apache.hadoop.mapreduce.lib.input FileInputFormat))
-	(:import (org.apache.hadoop.mapreduce.lib.output FileOutputFormat))
-  (:import (org.apache.hadoop.fs FileSystem Path))
-  (:import (org.apache.hadoop.io Text BytesWritable NullWritable
-                                 SequenceFile SequenceFile$CompressionType SequenceFile$Metadata))
-	(:import (org.apache.log4j Logger)))
+	(:import (com.linkedin.json LatestExpansionFunction)
+           (org.apache.hadoop.conf Configuration)
+           (org.apache.hadoop.mapreduce Job InputSplit TaskAttemptContext RecordWriter)
+	         (org.apache.hadoop.mapreduce.lib.input FileInputFormat FileSplit)
+	         (org.apache.hadoop.mapreduce.lib.output FileOutputFormat)
+           (org.apache.hadoop.fs FileSystem Path FSDataInputStream)
+           (org.apache.hadoop.io Text BytesWritable NullWritable
+                                 SequenceFile SequenceFile$CompressionType
+                                 SequenceFile$Metadata
+                                 SequenceFile$Writer)
+           (org.apache.avro.file SeekableInput)
+	         (org.apache.log4j Logger)))
 
 (def *logger* (Logger/getLogger "avro-storage"))
 
 ;
-; Util functions
-;
-
-(defn job-param
-  "Delayed evaluation for config params as input or output path
-   that are computed dynamically in job setup."
-  [param job]
-  (if (fn? param) (param (.getConfiguration job)) (str param)))
-
-;
 ; AvroRecordReader
 ;
+
+(defn- avro-seekable-input
+  "Seekable input for avro-reader."
+  [#^Path path #^Configuration conf]
+  (let [#^FileSystem        fs     (.getFileSystem path conf)
+        #^FSDataInputStream stream (.open fs path)
+                            len    (.getLength (.getContentSummary fs path))]
+    (proxy [java.io.Closeable SeekableInput] []
+      (length []            len)
+      (seek   [pos]         (.seek stream pos))
+      (tell   []            (.getPos stream))
+      (close  []            (.close stream))
+      (read   [barr off ln] (.read stream barr off ln)))))
 
 (gen-class
   :name       com.linkedin.mr-kluj.avro-storage.AvroRecordReader
   :prefix     arr-
   :state      state
   :init       init
-  :implements [clojure.lang.IDeref]
+  :implements []
   :extends    org.apache.hadoop.mapreduce.RecordReader)
 
 (defn arr-initialize
-	[this #^InputSplit split #^TaskAttemptContext context]
- (let [conf   (.getConfiguration context)
-       path   (.getPath split)
-       fs     (.getFileSystem path conf)
-       reader (avro-utils/avro-reader (.open fs path))]
-   (reset! (.state this) {:reader  reader :value nil})))
+	[#^com.linkedin.mr-kluj.avro-storage.AvroRecordReader this
+   #^FileSplit split #^TaskAttemptContext context]
+ (let [#^Configuration conf   (.getConfiguration context)
+                       fields (.get conf "li.avro.decode.fields")
+       #^Path          path   (.getPath split)
+       #^SeekableInput si     (avro-seekable-input path conf)
+                       reader  (avro-utils/avro-reader si (if fields (read-string fields)))]
+   (avro-utils/sync-pos reader (.getStart split))
+   (reset! (.state this) {:reader  reader
+                          :value nil
+                          :start (avro-utils/position reader)
+                          :end   (+ (.getStart split) (.length si))})))
 
 (defn arr-getCurrentKey
   [this]
   nil)
 
 (defn arr-getCurrentValue
-  [this]
-  (:value @this))
+  [#^com.linkedin.mr-kluj.avro-storage.AvroRecordReader this]
+  (:value @(.state this)))
 
 (defn arr-getProgress
-  [this]
-  0.0)
+  [#^com.linkedin.mr-kluj.avro-storage.AvroRecordReader this]
+  (let [state  @(.state this)
+        start  (:start state)
+        end    (:end state)
+        reader (:reader state)]
+    (float (if (= end start)
+             0.0
+             (min 1.0 (/ (- (avro-utils/position reader) start) (- end start)))))))
 
 (defn arr-nextKeyValue
-  [this]
-  (let [reader  (:reader @this)]
+  [#^com.linkedin.mr-kluj.avro-storage.AvroRecordReader this]
+  (let [reader  (:reader @(.state this))]
     (if (avro-utils/has-next reader)
       (let [value (avro-utils/read-next reader)]
         (swap! (.state this) assoc :value value)
@@ -81,17 +99,13 @@
       false)))
 
 (defn arr-close
-  [this]
-  (avro-utils/close (:reader @this))
+  [#^com.linkedin.mr-kluj.avro-storage.AvroRecordReader this]
+  (avro-utils/close (:reader @(.state this)))
   (reset! (.state this) nil))
 
 (defn arr-init
   []
   [[] (atom {})])
-
-(defn arr-deref
-  [this]
-  @(.state this))
 
 ;
 ; AvroInputFormat
@@ -119,12 +133,12 @@
   "Write data to a single avro file. This format is used when no key schema
    is provided to avro-storage-output. The file is written using avro
    DataFileWriter and can be read using DataFileReader."
-  [file-output-format context schema]
-  (let [conf   (.getConfiguration context)
-        path   (.getDefaultWorkFile file-output-format context "")
-        fs     (.getFileSystem path conf)
-        schema (avro/avro-schema schema)
-        writer (avro-utils/avro-writer (.create fs path) schema)]
+  [file-output-format #^TaskAttemptContext context schema]
+  (let [#^Configuration conf   (.getConfiguration context)
+        #^Path          path   (.getDefaultWorkFile file-output-format context "")
+        #^FileSystem    fs     (.getFileSystem path conf)
+        #^Schema        schema (avro/avro-schema schema)
+                        writer (avro-utils/avro-writer (.create fs path) schema)]
     (proxy [RecordWriter] []
       (write [key value]
              (avro-utils/write writer value)
@@ -135,17 +149,17 @@
 (defn- custom-sequence-writer
   "Writes data to a Sequence file and serializes key and values
    using avro and provided key and value schema."
-  [file-output-format context key-schema value-schema]
-  (let [conf         (.getConfiguration context)
-        path         (.getDefaultWorkFile file-output-format context "")
-        fs           (.getFileSystem path conf)
-        meta         (doto (SequenceFile$Metadata.)
-                       (.set (Text. "key.schema") (Text. key-schema))
-                       (.set (Text. "value.schema") (Text. value-schema)))
-        writer       (SequenceFile/createWriter fs conf path
-                         (.getOutputKeyClass context)
-                         (.getOutputValueClass context)
-                         SequenceFile$CompressionType/NONE, nil, context, meta)]
+  [file-output-format #^TaskAttemptContext context #^String key-schema #^String value-schema]
+  (let [#^Configuration conf         (.getConfiguration context)
+        #^Path          path         (.getDefaultWorkFile file-output-format context "")
+        #^FileSystem    fs           (.getFileSystem path conf)
+                        meta         (doto (SequenceFile$Metadata.)
+                                       (.set (Text. "key.schema") (Text. key-schema))
+                                       (.set (Text. "value.schema") (Text. value-schema)))
+        #^SequenceFile$Writer writer (SequenceFile/createWriter fs conf path
+                                                                (.getOutputKeyClass context)
+                                                                (.getOutputValueClass context)
+                                                                SequenceFile$CompressionType/NONE, nil, context, meta)]
     (proxy [RecordWriter] []
       (write [key value]
              (.append writer
@@ -177,14 +191,17 @@
     "Avro file storage input. Provide file location or a job-param function which
      takes the job Configuration as input parameter and returns avro file location.
      The function can be used to determine input file location dynamically in the setup stage.
-     Records are provided as value to the mapper, key is always nil."
-    [path]
+     Records are provided as value to the mapper, key is always nil.
+     Optionally provide fields to decode from the recode. If no fields are provided,
+     entire record will be decoded."
+    [path & fields]
     (when (nil? path) (throw (RuntimeException. "Input path cannot be null.")))
     (job/add-config
       (fn [#^Job job]
-        (let [path         (job-param path job)
-              actual-paths (str-utils/join "," (map latest-exp-fn (str-utils/split path #",")))]
+        (let [actual-paths (str-utils/join "," (map latest-exp-fn (str-utils/split path #",")))]
           (.info #^Logger *logger* (format "Avro Input: Given paths[%s] which resolved to paths[%s]" path actual-paths))
+          (when (-> fields empty? not)
+            (.set (.getConfiguration job) "li.avro.decode.fields" (with-out-str (print fields))))
           (doto job
             (.setInputFormatClass com.linkedin.mr-kluj.avro-storage.AvroInputFormat)
             (FileInputFormat/addInputPaths #^String actual-paths)))))))
@@ -213,14 +230,13 @@
   (job/add-config
     (fn [#^Job job]
       (when (nil? path) (throw (RuntimeException. (format "Output on job[%s] cannot be null." (.getJobName job)))))
-      (let [path (job-param path job)]
-	      (doto job
-	        (.setOutputKeyClass (if key-schema BytesWritable NullWritable))
-	        (.setOutputValueClass BytesWritable)
-	        (.setOutputFormatClass com.linkedin.mr-kluj.avro-storage.AvroOutputFormat)
-	        (FileOutputFormat/setOutputPath (Path. path)))
+      (doto job
+        (.setOutputKeyClass (if key-schema BytesWritable NullWritable))
+        (.setOutputValueClass BytesWritable)
+        (.setOutputFormatClass com.linkedin.mr-kluj.avro-storage.AvroOutputFormat)
+        (FileOutputFormat/setOutputPath (Path. path)))
 	      (when key-schema (.set (.getConfiguration job) "output.key.schema" (avro/json-schema key-schema)))
-	      (when value-schema (.set (.getConfiguration job) "output.value.schema" (avro/json-schema value-schema))))
+	      (when value-schema (.set (.getConfiguration job) "output.value.schema" (avro/json-schema value-schema)))
       job)))
 
 ;
@@ -240,4 +256,5 @@
 	    (job/create-reducer (fn [key values context] [[nil {"first" key "count" (count values)}]]))
 	    (avro/avro-intermediate-data avro-string avro-int)
 	    (avro/avro-storage-output "count.avro" nil NameCount)))
+  
 )
